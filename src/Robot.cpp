c7e4de58c6ae7a1d9b58ecf76afb6c846c3b3ee9
dvdbot <dylan@vossystems.nl>
2017-01-16 17:35:37 +0100

    Edited based on change in IRLibrary

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,3 +1,6 @@
+/*
+ * 2016
+ * */
 #include <iostream>
 #include <memory>
 #include <string>
@@ -12,19 +15,9 @@
 #include <SmartDashboard/SmartDashboard.h>
 #include <Timer.h>
 
-/**
- * This is a demo program showing the use of the RobotDrive class.
- * The SampleRobot class is the base of a robot application that will automatically call your
- * Autonomous and OperatorControl methods at the right time as controlled by the switches on
- * the driver station or the field controls.
- *
- * WARNING: While it may look like a good choice to use for your code if you're inexperienced,
- * don't. Unless you know what you are doing, complex code will be much more difficult under
- * this system. Use IterativeRobot or Command-Based instead if you're new.
- */
 class Robot: public frc::SampleRobot
 {
-	IR::IRRobotDrive 	myDrive {0, 1, 2, 3};
+	IR::IRRobotDrive 	myDrive {0, 1, 2, 3, IR::Tank};
 	IR::IRJoystick 		joystick {0},
 						gamePad {1};
 	IRShooter 			irshooter {4, 5, 6};
